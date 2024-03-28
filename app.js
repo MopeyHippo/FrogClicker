c5f79e502886260a941aca459538b84636161c33
MopeyHippo <connor.james94@gmail.com>
2024-03-28 16:57:18 +0000

    first build wave

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
-const form = document.querySelector("form");
+const theButton = document.getElementById("frogButton");
+const frogSpan = document.getElementById("frogSpan");
+const fpsSpan = document.getElementById("fpsSpan");
+const tankButton = document.getElementById("tankButton");
+const vivButton = document.getElementById("vivButton");
+const stats = {
+  frogCount: 0,
+  fps: 0,
+};
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
+const storageStats = JSON.parse(localStorage.getItem("stats"));
 
-  const formData = new FormData(form);
-  const colour = formData.get("colour");
+if (storageStats !== null) {
+  stats.frogCount = storageStats.frogCount;
+  stats.fps = storageStats.fps;
+}
 
-  localStorage.setItem("colour", colour);
-});
+function buyFrogs() {
+  stats.frogCount++;
+  updatePage();
+  updateStorage();
+}
+function buyTank() {
+  stats.fps += 1;
+  stats.frogCount -= 10;
+  updatePage();
+  updateStorage();
+}
+function buyViv() {
+  stats.fps += 10;
+  stats.frogCount -= 100;
+  updatePage();
+  updateStorage();
+}
+
+function updatePage() {
+  frogSpan.textContent = stats.frogCount;
+  fpsSpan.textContent = stats.fps;
+}
+
+function updateStorage() {
+  localStorage.setItem("stats", JSON.stringify(stats));
+}
+
+function reset() {
+  stats.fps = 0;
+  stats.frogCount = 0;
+}
+
+frogButton.addEventListener("click", buyFrogs);
+tankButton.addEventListener("click", buyTank);
+vivButton.addEventListener("click", buyViv);
+resetButton.addEventListener("click", reset);
+
+setInterval(function () {
+  stats.frogCount += stats.fps;
+  updatePage();
+  updateStorage();
+}, 1000);
