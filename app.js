1f0847ab8d39d3c15d25ac37b783dcaffadcaca8
MopeyHippo <connor.james94@gmail.com>
2024-03-28 10:20:17 +0000

    initial upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const colour = formData.get("colour");
+
+  localStorage.setItem("colour", colour);
+});
